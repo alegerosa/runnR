bda03aa61f8d84966fdb2ef162396cda68087d7a
Ale Gerosa <alegerosa@gmail.com>
2020-02-15 15:41:27 -0500

    Add submit button and delayed reactivity. Add min/mi to mi/km conversion. Add title format.

--- app/app.R
+++ app/app.R
@@ -2,20 +2,39 @@ library(shiny)
 library(runnR)
 
 ui <- fluidPage(
-  "Convert min/km to min/mile",
-  textInput(inputId = "pace_km", "Your pace in minutes per kilometer", value = "7:00"),
-  textOutput(outputId = "pace_mi_output")
+  tags$h1("Pace converter"),
+  textInput(inputId = "pace_km", "Your pace in minutes per kilometer"),
+  actionButton(inputId = "pace_km_submit", label = "Convert to min/mile"),
+  textOutput(outputId = "pace_mi_output"),
+  tags$br(),
+  textInput(inputId = "pace_mi", "Your pace in minutes per mile"),
+  actionButton(inputId = "pace_mi_submit", label = "Convert to min/km"),
+  textOutput(outputId = "pace_km_output")
 )
 
 
 
 
 server <- function(input, output) {
+  pace_km <- eventReactive(input$pace_km_submit, {
+    input$pace_km
+  })
   output$pace_mi_output <- renderText({
     paste(
       "Your pace is ",
-      pace_km_to_pace_mi(input$pace_km),
+      pace_km_to_pace_mi(pace_km()),
       " minutes per mile")
+
+  })
+
+  pace_mi <- eventReactive(input$pace_mi_submit, {
+    input$pace_mi
+  })
+  output$pace_km_output <- renderText({
+    paste(
+      "Your pace is ",
+      pace_mi_to_pace_km(pace_mi()),
+      " minutes per kilometer")
 
   })
 }
