70792b1c5d3031d286d0c0e1e30d440070a61e69
Ale Gerosa <alegerosa@gmail.com>
2020-02-16 13:42:59 -0500

    Add functions that take in two numbers for minutes and seconds. Add numberic inputs to shiny app.

--- R/pace_converter_functions.R
+++ R/pace_converter_functions.R
@@ -1,77 +1,145 @@
-#' Pace km to pace mi
+#' Pace km to pace mi, text
 #'
-#' This function converts paces from minutes per kilometer to minutes per mile
-#' @param pace_km A string or vector of strings with pace in minutes per kilometer, where the minutes are separated from the seconds by a colon.
+#' This function converts paces from minutes per kilometer to minutes per mile, taking strings as inputs and returning strings as outputs
+#' @param pace_km_text A string or vector of strings with pace in minutes per kilometer, where the minutes are separated from the seconds by a colon.
 #' @return A string or vector of strings with the pace in minutes per mile, where the minutes are separated from the seconds by a colon.
 #' @examples
-#' pace_km_to_pace_mi("6:03")
-#' pace_km_to_pace_kmi(c("7:30", "5:27", "6:42"))
+#' pace_km_to_pace_mi_text("6:03")
+#' pace_km_to_pace_mi_text(c("7:30", "5:27", "6:42"))
 #'
 #' @export
-pace_km_to_pace_mi <- function(pace_km) {
+pace_km_to_mi_text <- function(pace_km_text) {
 
   if(
-    !(is.character(pace_km) &&
-      stringr::str_detect(pace_km, pattern = ":"))
+    !(is.character(pace_km_text) &&
+      stringr::str_detect(pace_km_text, pattern = ":"))
   ) {
     stop('This function only works with the paces inputed as strings of minutes and seconds separated by a \':\', check that all your inputs meet this format: \'6:45\'')
   }
 
   dur_km <- lubridate::duration(
-    mins = as.numeric(stringr::word(pace_km, 1, sep = stringr::fixed(":"))),
-    seconds = as.numeric(stringr::word(pace_km, 2, sep = stringr::fixed(":")))
+    mins = as.numeric(stringr::word(pace_km_text, 1, sep = stringr::fixed(":"))),
+    seconds = as.numeric(stringr::word(pace_km_text, 2, sep = stringr::fixed(":")))
   )
 
   dur_mi <- dur_km * 1.609344
 
   pace_mi_period <- lubridate::as.period(dur_mi)
 
-  pace_mi <- paste(
+  pace_mi_text <- paste(
     lubridate::minute(pace_mi_period),
     ":",
     stringr::str_pad(floor(lubridate::second(pace_mi_period)), 2, "left", pad = "0"),
     sep = ""
   )
-  return(pace_mi)
+  return(pace_mi_text)
 }
 
-pace_km_to_pace_mi(c("2:59", NA))
+pace_km_to_mi_text(c("2:59", NA))
 
-#' Pace mi to pace km
+#' Pace mi to pace km, text
 #'
-#' This function converts paces from minutes per mile to minutes per kilometer
-#' @param pace_mi A string or vector of strings with pace in minutes per mile, where the minutes are separated from the seconds by a colon.
+#' This function converts paces from minutes per mile to minutes per kilometer, taking strings as inputs and returning strings as outputs
+#' @param pace_mi_text A string or vector of strings with pace in minutes per mile, where the minutes are separated from the seconds by a colon.
 #' @return A string or vector of strings with the pace in minutes per kilometer, where the minutes are separated from the seconds by a colon.
 #' @examples
-#' pace_mi_to_pace_km("7:45")
-#' pace_mi_to_pace_km(c("11:02", "9:00", "10:34"))
+#' pace_mi_to_km_text("7:45")
+#' pace_mi_to_km_text(c("11:02", "9:00", "10:34"))
 #'
 #' @export
-pace_mi_to_pace_km <- function(pace_mi) {
+pace_mi_to_km_text <- function(pace_mi_text) {
 
   if(
-    !(is.character(pace_mi) &&
-      stringr::str_detect(pace_mi, pattern = ":"))
+    !(is.character(pace_mi_text) &&
+      stringr::str_detect(pace_mi_text, pattern = ":"))
   ) {
     stop('This function only works with the paces inputed as strings of minutes and seconds separated by a \':\', check that all your inputs meet this format: \'6:45\'')
   }
 
   dur_mi <- lubridate::duration(
-    mins = as.numeric(stringr::word(pace_mi, 1, sep = stringr::fixed(":"))),
-    seconds = as.numeric(stringr::word(pace_mi, 2, sep = stringr::fixed(":")))
+    mins = as.numeric(stringr::word(pace_mi_text, 1, sep = stringr::fixed(":"))),
+    seconds = as.numeric(stringr::word(pace_mi_text, 2, sep = stringr::fixed(":")))
   )
 
   dur_km <- dur_mi / 1.609344
 
   pace_km_period <- lubridate::as.period(dur_km)
 
-  pace_km <- paste(
+  pace_km_text <- paste(
     lubridate::minute(pace_km_period),
     ":",
     stringr::str_pad(floor(lubridate::second(pace_km_period)), 2, "left", pad = "0"),
     sep = ""
   )
-  return(pace_km)
+  return(pace_km_text)
 }
 
-pace_mi_to_pace_km(c("8:40", "4:50"))
+pace_mi_to_km_text(c("8:40", "4:50"))
+
+#' Pace km to pace mi, number input
+#'
+#' This function converts a pace from minutes per kilometer to minutes per mile, taking two numbers as inputs (minutes and seconds) and returning a string as output. Only works with one observation at a time for now.
+#' @param pace_km_min A number with the whole minutes in the pace in minute per kilometer
+#' @param pace_km_sec A number with the seconds in the pace in minute per kilometer
+#' @return A string with the pace in minutes per mile, where the minutes are separated from the seconds by a colon.
+#' @examples
+#' pace_km_to_mi_num_input(7,30)
+#' pace_km_to_mi_num_input(5,27)
+#'
+#' @export
+pace_km_to_mi_num_input <- function(pace_km_min, pace_km_sec) {
+
+  dur_km <- lubridate::duration(
+    mins = pace_km_min,
+    seconds = pace_km_sec
+  )
+
+  dur_mi <- dur_km * 1.609344
+
+  pace_mi_period <- lubridate::as.period(dur_mi)
+
+  pace_mi_text <- paste(
+    lubridate::minute(pace_mi_period),
+    ":",
+    stringr::str_pad(floor(lubridate::second(pace_mi_period)), 2, "left", pad = "0"),
+    sep = ""
+  )
+  return(pace_mi_text)
+}
+
+pace_km_to_mi_num_input(7,30)
+pace_km_to_mi_num_input(5,27)
+
+#' Pace mi to pace km, number input
+#'
+#' This function converts a pace from minutes per mile to minutes per kilometer, taking two numbers as inputs (minutes and seconds) and returning a string as output. Only works with one observation at a time for now.
+#' @param pace_mi_min A number with the whole minutes in the pace in minute per mi
+#' @param pace_mi_sec A number with the seconds in the pace in minute per mi
+#' @return A string with the pace in minutes per kilometer, where the minutes are separated from the seconds by a colon.
+#' @examples
+#' pace_mi_to_km_num_input(7,30)
+#' pace_mi_to_km_num_input(5,27)
+#'
+#' @export
+pace_mi_to_km_num_input <- function(pace_mi_min, pace_mi_sec) {
+
+  dur_mi <- lubridate::duration(
+    mins = pace_mi_min,
+    seconds = pace_mi_sec
+  )
+
+  dur_km <- dur_mi / 1.609344
+
+  pace_km_period <- lubridate::as.period(dur_km)
+
+  pace_km_text <- paste(
+    lubridate::minute(pace_km_period),
+    ":",
+    stringr::str_pad(floor(lubridate::second(pace_km_period)), 2, "left", pad = "0"),
+    sep = ""
+  )
+  return(pace_km_text)
+}
+
+pace_mi_to_km_num_input(7,30)
+pace_mi_to_km_num_input(5,27)
--- app/app.R
+++ app/app.R
@@ -2,41 +2,74 @@ library(shiny)
 library(runnR)
 
 ui <- fluidPage(
-  tags$h1("Pace converter"),
-  textInput(inputId = "pace_km", "Your pace in minutes per kilometer"),
-  actionButton(inputId = "pace_km_submit", label = "Convert to min/mile"),
-  textOutput(outputId = "pace_mi_output"),
-  tags$br(),
-  textInput(inputId = "pace_mi", "Your pace in minutes per mile"),
-  actionButton(inputId = "pace_mi_submit", label = "Convert to min/km"),
-  textOutput(outputId = "pace_km_output")
+  h1("Pace converter"),
+  fluidRow(
+    column(
+      width = 6,
+      align = "center",
+      h4("Input the minutes and seconds of your pace in minutes per kilometer", align = "left"),
+      fluidRow(
+        column(width = 6,
+               align = "right",
+               numericInput(inputId = "pace_km_min", label = "Minutes", value = 7, min = 0, max = 60, step = 1, width = "50%")
+               ),
+        column(width = 6,
+               align = "left",
+               numericInput(inputId = "pace_km_sec", label = "Seconds", value = 10, min = 0, max = 60, step = 1, width = "50%")
+               )
+        ),
+      actionButton(inputId = "pace_km_submit", label = "Convert to min/mile"),
+      h3(textOutput(outputId = "pace_mi_output")),
+      hr()),
+    column(
+      width = 6,
+      align = "center",
+      h4("Input the minutes and seconds of your pace in minutes per mile", align = "left"),
+      fluidRow(
+        column(width = 6,
+               align = "right",
+               numericInput(inputId = "pace_mi_min", label = "Minutes", value = 7, min = 0, max = 60, step = 1, width = "50%")
+        ),
+        column(width = 6,
+               align = "left",
+               numericInput(inputId = "pace_mi_sec", label = "Seconds", value = 10, min = 0, max = 60, step = 1, width = "50%")
+        )
+      ),
+      actionButton(inputId = "pace_mi_submit", label = "Convert to min/km"),
+      h3(textOutput(outputId = "pace_km_output")),
+      hr())
+      )
 )
 
 
-
-
 server <- function(input, output) {
-  pace_km <- eventReactive(input$pace_km_submit, {
-    input$pace_km
+  pace_km_min <- eventReactive(input$pace_km_submit, {
+    input$pace_km_min
+  })
+  pace_km_sec <- eventReactive(input$pace_km_submit, {
+    input$pace_km_sec
   })
   output$pace_mi_output <- renderText({
     paste(
       "Your pace is ",
-      pace_km_to_pace_mi(pace_km()),
+      pace_km_to_mi_num_input(pace_km_min(), pace_km_sec()),
       " minutes per mile")
-
   })
 
-  pace_mi <- eventReactive(input$pace_mi_submit, {
-    input$pace_mi
+  pace_mi_min <- eventReactive(input$pace_mi_submit, {
+    input$pace_mi_min
+  })
+  pace_mi_sec <- eventReactive(input$pace_mi_submit, {
+    input$pace_mi_sec
   })
   output$pace_km_output <- renderText({
     paste(
       "Your pace is ",
-      pace_mi_to_pace_km(pace_mi()),
+      pace_mi_to_km_num_input(pace_mi_min(), pace_mi_sec()),
       " minutes per kilometer")
 
   })
 }
+
 
 shinyApp(ui = ui, server = server)
