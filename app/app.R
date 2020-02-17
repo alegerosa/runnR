9972834d390f5666c4eb9b2b1ae560f794061af5
Ale Gerosa <alegerosa@gmail.com>
2020-02-16 23:19:08 -0500

    Change input to minutes and seconds selector and functions to numeric input fuctions. Make layout a bit more intentional.

--- app/app.R
+++ app/app.R
@@ -2,52 +2,45 @@ library(shiny)
 library(runnR)
 
 ui <- fluidPage(
-  h1("Pace converter"),
   fluidRow(
     column(
       width = 6,
       align = "center",
-      h4("Input the minutes and seconds of your pace in minutes per kilometer", align = "left"),
+      h1("Pace Converter"),
+      h4("Input the minutes and seconds of your pace in minutes per kilometer"),
       fluidRow(
         column(width = 6,
-               align = "right",
-               numericInput(inputId = "pace_km_min", label = "Minutes", value = 7, min = 0, max = 60, step = 1, width = "50%")
+               selectInput(inputId = "pace_km_min", label = "Minutes", choices = as.list(1:25), selected = 7, width = "50%")
                ),
         column(width = 6,
-               align = "left",
-               numericInput(inputId = "pace_km_sec", label = "Seconds", value = 10, min = 0, max = 60, step = 1, width = "50%")
+               selectInput(inputId = "pace_km_sec", label = "Seconds", choices = as.list(0:60), selected = 10, width = "50%")
                )
         ),
       actionButton(inputId = "pace_km_submit", label = "Convert to min/mile"),
       h3(textOutput(outputId = "pace_mi_output")),
-      hr()),
-    column(
-      width = 6,
-      align = "center",
-      h4("Input the minutes and seconds of your pace in minutes per mile", align = "left"),
+      hr(),
+      h4("Input the minutes and seconds of your pace in minutes per mile"),
       fluidRow(
         column(width = 6,
-               align = "right",
-               numericInput(inputId = "pace_mi_min", label = "Minutes", value = 7, min = 0, max = 60, step = 1, width = "50%")
+               selectInput(inputId = "pace_mi_min", label = "Minutes", choices = as.list(1:25), selected = 11, width = "50%")
         ),
         column(width = 6,
-               align = "left",
-               numericInput(inputId = "pace_mi_sec", label = "Seconds", value = 10, min = 0, max = 60, step = 1, width = "50%")
+               selectInput(inputId = "pace_mi_sec", label = "Seconds", choices = as.list(0:60), selected = 32, width = "50%")
         )
       ),
       actionButton(inputId = "pace_mi_submit", label = "Convert to min/km"),
       h3(textOutput(outputId = "pace_km_output")),
       hr())
-      )
-)
+      ))
+
 
 
 server <- function(input, output) {
   pace_km_min <- eventReactive(input$pace_km_submit, {
-    input$pace_km_min
+    as.numeric(input$pace_km_min)
   })
   pace_km_sec <- eventReactive(input$pace_km_submit, {
-    input$pace_km_sec
+    as.numeric(input$pace_km_sec)
   })
   output$pace_mi_output <- renderText({
     paste(
@@ -57,10 +50,10 @@ server <- function(input, output) {
   })
 
   pace_mi_min <- eventReactive(input$pace_mi_submit, {
-    input$pace_mi_min
+    as.numeric(input$pace_mi_min)
   })
   pace_mi_sec <- eventReactive(input$pace_mi_submit, {
-    input$pace_mi_sec
+    as.numeric(input$pace_mi_sec)
   })
   output$pace_km_output <- renderText({
     paste(
